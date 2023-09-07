1f5e59c27bb718f0c86a886e5a80fb7f5f108ced
Xavier Hreben <projectfigirl@gmail.com>
2023-09-07 11:37:53 -0400

    Added sorting and something else

---
+++ DSA/Recursion/Fib/Fib/Program.cs
@@ -0,0 +1,58 @@
+﻿// fibonacci
+// fib of a number = sum of fib of previous 2 numbers
+// fib 0 = 0 fib 1 = 1
+
+// recurse through until 0 and add that number to previous fib number
+Dictionary<int, int> computedValues = new();
+//Console.WriteLine(fibDance(8));
+
+foreach (var item in fibDance2(100)) {
+    if (item <= 0) break;
+    Console.WriteLine(item);
+}
+
+int fibDance(int num) {
+    if (num == 0) { return 0; }
+    if (num == 1) { return 1; }
+
+    int num1 = num - 1;
+    int num2 = num - 2;
+    int fibNum1 = 0;
+    int fibNum2 = 0;
+
+    if (computedValues.ContainsKey(num1)) {
+        fibNum1 = computedValues[num1];
+    } else {
+        fibNum1 = fibDance(num1);
+        computedValues.Add(num1, fibNum1);
+    }
+
+    if (computedValues.ContainsKey(num2)) {
+        fibNum2 = computedValues[num2];
+    } else {
+        fibNum2 = fibDance(num2);
+        computedValues.Add(num2, fibNum2);
+    }
+
+    return fibNum1 + fibNum2;
+}
+
+
+// iterative
+// iterate up to given num
+// fib num = curr + prev?
+static IEnumerable<int> fibDance2(int target) {
+    int fibNum = 1;
+    int currFib = 1;
+    int prevFib = 0;
+    
+    for (int i = 1; i < target; i++) {
+        fibNum = currFib + prevFib;
+        yield return fibNum;
+        prevFib = currFib;
+        currFib = fibNum;
+    }
+
+ //   return fibNum;
+
+}
---
+++ DSA/Sort/Sorting/Bubble Sort/Program.cs
@@ -0,0 +1,29 @@
+﻿// bubble sort
+// look at 2 elements at a time
+// compare the 2 elements
+// swap the 2 elements if left < right
+// left and right pointer equal to 0 and 1 respectively
+// every pass minus the amount of passes we have to make by 1
+// array length = 10
+// after first pass reduce array length target by 1
+
+// if pass doesnt swap anything we are done sorting
+
+int[] ourAry = new int[] { 12, 24, 3, 7, 2, 5, 43, 9, 20, 27, 18 };
+bool doneSwapping = false;
+
+while (!doneSwapping) {
+
+    doneSwapping = true;
+
+    for (int i = 1; i < ourAry.Length; i++) {
+        if (ourAry[i] < ourAry[i - 1]) {
+            (ourAry[i], ourAry[i - 1]) = (ourAry[i - 1], ourAry[i]);
+            doneSwapping = false;
+        }
+    }
+
+}
+
+Console.WriteLine(String.Join(",", ourAry));
+
